cee63ef86e302cdd9ebe49701bc873f4ccf09f2a
stripuramallu3 <stripuramallu3@gatech.edu>
2017-12-11 08:53:18 -0500

    Update listener_tweepy.py

--- listener_tweepy.py
+++ listener_tweepy.py
@@ -4,10 +4,10 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 from kafka import SimpleProducer,KafkaClient 
 #Get Twitter API keys from Twitter developer account
-access_token = "772234465057931264-OWIX3wDgn3r77wdN0Y1UM95rKGALc1D"
-access_token_secret = "GqGZNoVZO8RuODsU7wFI7jhSJtui3ooJRDQ7d32iS1Coh"
-consumer_secret = "gAwD1X8XdNwnl2X11TsZmYay3b9jFcvP5hQNlbLJVZBIw1Vbgb"
-consumer_key = "yWCC61dsZVHFkYsucAGCJJa5o"
+access_token = ""
+access_token_secret = ""
+consumer_secret = ""
+consumer_key = ""
 
 
 
